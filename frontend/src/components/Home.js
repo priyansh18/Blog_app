77802317121a3a8949c2b36e8bf8b078af4a55ca
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-12 22:21:30 +0530

    Fetchimg Data from django-rest-api

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1 class="display-4">Welcome to Blogs</h1>
+        <p class="lead">
+          We make all kinds of awesome blogs about various topics.
+        </p>
+        <hr class="my-4" />
+        <p>Click the button below to check out our blogs.</p>
+        <Link class="btn btn-primary btn-lg" to="/blog" role="button">
+          Check it out
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
