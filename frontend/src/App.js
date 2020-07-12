3a88e34cd3d397b9e1feb135028b0b502264bafd
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-12 19:38:24 +0530

    Setting up Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from "./hocs/Layout";
+import Home from "./components/Home";
+import Category from "./components/Category";
+import Blog from "./components/Blog";
+import BlogDetail from "./components/BlogDetail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/blog" component={Blog} />
+            <Route exact path="/category/:id" component={Category} />
+            <Route exact path="/blog/:id" component={BlogDetail} />
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return <nav>Navbar</nav>;
+};
+
+export default Navbar;
