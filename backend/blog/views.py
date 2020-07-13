51a4ae9fe4819d64438315ea54e0610c7db557af
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-07-14 00:25:04 +0530

    Filtering posts based on category

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -2,38 +2,47 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView,RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 # Create your views here.
 
+
 class BlogPostListView(ListAPIView):
-  queryset = BlogPost.objects.order_by('-date_created')
-  serializer_class = BlogPostSerializer
-  lookup_field = 'slug'
-  permission_classes = (permissions.AllowAny,)
+    queryset = BlogPost.objects.order_by('-date_created')
+    serializer_class = BlogPostSerializer
+    lookup_field = 'slug'
+    permission_classes = (permissions.AllowAny,)
+
 
 class BlogPostDetailView(RetrieveAPIView):
-  queryset = BlogPost.objects.order_by('-date_created')
-  serializer_class = BlogPostSerializer
-  lookup_field = 'slug'
-  permission_classes = (permissions.AllowAny,)
+    queryset = BlogPost.objects.order_by('-date_created')
+    serializer_class = BlogPostSerializer
+    lookup_field = 'slug'
+    permission_classes = (permissions.AllowAny,)
+
 
 class BlogPostFeaturedView(ListAPIView):
-  queryset = BlogPost.objects.all().filter(featured = True)
-  serializer_class = BlogPostSerializer
-  lookup_field = 'slug'
-  permission_classes = (permissions.AllowAny,) 
+    queryset = BlogPost.objects.all().filter(featured=True)
+    serializer_class = BlogPostSerializer
+    lookup_field = 'slug'
+    permission_classes = (permissions.AllowAny,)
+
 
 class BlogPostCategoryView(APIView):
-  serializer_class = BlogPostSerializer
-  permission_classes = (permissions.AllowAny,)  
+    serializer_class = BlogPostSerializer
+    permission_classes = (permissions.AllowAny,)
 
-  def posts(self,request,format=None):
-    data = self.request.data
-    category = data['category']
-    queryset = BlogPost.objects.order_by('-date_created').filter(category__iexact=category)
+    def post(self, request, format=None):
+        data = self.request.data
+        # print("Data", data)
+        category = data['category']
+        # print("Category", category)
+        queryset = BlogPost.objects.order_by(
+            '-date_created').filter(category__iexact=category)
 
-    serializer = BlogPostSerializer(queryset,many=True)
+        serializer = BlogPostSerializer(queryset, many=True)
 
-    return Response(serializer.data)
+        # print("Response",serializer.data)
+
+        return Response(serializer.data)
